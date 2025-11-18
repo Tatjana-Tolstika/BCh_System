883e5cd3e0c4970d9e7cba1963ed50a953e24439
Tatjana-Tolstika <tatjana.tolstika@gmail.com>
2025-11-18 18:12:32 +0200

    TestTask class added

---
+++ src/main/java/lv/venta/model/TestTask.java
@@ -0,0 +1,45 @@
+package lv.venta.model;
+
+import java.util.Collection;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name="Study courses")
+@Entity
+public class TestTask {
+	@Setter(value = AccessLevel.NONE)
+	@Column(name = "TestTasks")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long taskID;
+	
+	@NotNull
+	@Pattern(regexp="[A-za-z.,_:; ]{5,150}")
+	@Column(name = "TaskDescription")
+	private String taskDescription;
+	
+	@NotNull
+	@Column(name="MaxPointsAmount")
+	private int maxPoints;
+	
+	public TestTask(String description, int points) {
+		setTaskDescription(description);
+		setMaxPoints(points);
+	}
+}

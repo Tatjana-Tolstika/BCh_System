981f240ee65b887eaa5969809f2305ef576726a8
Tatjana-Tolstika <tatjana.tolstika@gmail.com>
2026-01-04 21:14:50 +0200

    Added creating functionality to LecturersCRUDController & created html file for that

--- src/main/java/lv/venta/controller/LecturersCRUDController.java
+++ src/main/java/lv/venta/controller/LecturersCRUDController.java
@@ -5,9 +5,12 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.validation.Valid;
 import lv.venta.model.Lecturers;
 import lv.venta.service.ILecturersCRUDService;
 
@@ -16,6 +19,29 @@ import lv.venta.service.ILecturersCRUDService;
 public class LecturersCRUDController {
 	@Autowired 
 	private ILecturersCRUDService lecturersService;
+	
+	//----------------------ADD---------------------------------------
+		@GetMapping("/add") //localhost:8081/lecturers/crud/add
+		public String getControllerAddNewLecturer(Model model) {
+			model.addAttribute("lecturers", new Lecturers());
+			return "create-lecturer";
+		}
+		
+		@PostMapping("/add")
+		public String postConstrollerAddNewStudent(@Valid Lecturers lecturer, BindingResult result, Model model) {
+			if(result.hasErrors()) {
+				return "create-lecturer";
+			}
+			try {
+				lecturersService.CreateLecturer(lecturer.getLecturerName(), lecturer.getLecturerSurname(), lecturer.getLecturerDegree());
+			}
+			catch(Exception e) {
+				model.addAttribute("package", e.getMessage());
+				return "show-error";
+			}
+			return "redirect:/lecturers/crud/all";
+		}
+		//----------------------------------------------------------------
 	
 	@GetMapping("/all") //localhost:8081/lecturers/crud/all
 	public String getConstrollerGetAllStudents(Model model) {
@@ -30,3 +56,15 @@ public class LecturersCRUDController {
 		}
 	}
 }
+//======================================CRUD=======================================================
+	//-------------------------------CREATE-----------------------------------
+	
+	//-----------------------------------------------------------------------
+	//------------------------------DELETE---------------------------------
+	//-----------------------------------------------------------------------
+	//-----------------------------RETRIEVE-----------------------------------
+	//-----------------------------------------------------------------------
+	//--------------------------UPDATE---------------------------------------
+	//-----------------------------------------------------------------------
+	
+	//=================================================================================================

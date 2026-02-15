97edad62c7d9097edac87c862bd541ed0fc01ce6
Tatjana-Tolstika <tatjana.tolstika@gmail.com>
2026-02-15 15:39:26 +0200

    Add: function to see all tests of the selected course

--- src/main/java/lv/venta/service/ILectViewService.java
+++ src/main/java/lv/venta/service/ILectViewService.java
@@ -2,10 +2,13 @@ package lv.venta.service;
 
 import java.util.List;
 
+import lv.venta.model.CourseTests;
 import lv.venta.model.StudyCourses;
 
 public interface ILectViewService {
 
 	public abstract List<StudyCourses> allCoursesForLecturer(long lectId) throws Exception;
+
+	public abstract List<CourseTests> allTestsByCourse(long courseId) throws Exception;
 
 }
--- src/main/java/lv/venta/service/impl/LectViewServiceImpl.java
+++ src/main/java/lv/venta/service/impl/LectViewServiceImpl.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lv.venta.model.CourseTests;
 import lv.venta.model.Lecturers;
 import lv.venta.model.StudyCourses;
+import lv.venta.repo.ICourseTestRepo;
 import lv.venta.repo.ILecturersRepo;
 import lv.venta.repo.IStudyCourseRepo;
 import lv.venta.service.ILectViewService;
@@ -17,6 +19,8 @@ public class LectViewServiceImpl implements ILectViewService{
 	private ILecturersRepo lecturersRepo;
 	@Autowired 
 	private IStudyCourseRepo coursesRepo;
+	@Autowired
+	private ICourseTestRepo testRepo;
 	
 	@Override
 	public List<StudyCourses> allCoursesForLecturer(long lectId) throws Exception{
@@ -31,5 +35,15 @@ public class LectViewServiceImpl implements ILectViewService{
 	    return courses;
 	}
 	
-
+	@Override
+	public List<CourseTests> allTestsByCourse(long courseId) throws Exception{
+		StudyCourses course = coursesRepo.findById(courseId)
+				.orElseThrow(()-> new Exception("Course not found!"));
+		List<CourseTests> tests = testRepo.findByCourse(course);
+		if(tests.isEmpty()) {
+			throw new Exception("This course has no tests!");
+		}
+		return tests;
+		
+	}
 }

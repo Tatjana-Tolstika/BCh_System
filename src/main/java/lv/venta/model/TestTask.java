17f29bbb9194fa8a12ccf82b4d1f46724d3841b9
Tatjana-Tolstika <tatjana.tolstika@gmail.com>
2026-02-16 14:16:19 +0200

    Something wasn't saved in testTask model class

--- src/main/java/lv/venta/model/TestTask.java
+++ src/main/java/lv/venta/model/TestTask.java
@@ -14,7 +14,6 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Null;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;

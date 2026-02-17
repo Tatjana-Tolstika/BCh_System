c898aa0a5bf807b0913faa39bd3d30ba1e8b99e0
Tatjana-Tolstika <tatjana.tolstika@gmail.com>
2026-02-18 00:12:59 +0200

    Add: new controller for tests of the course & made html ffile lecturer-courseTests , html file will be changed (design)

--- src/main/java/lv/venta/controller/LectViewController.java
+++ src/main/java/lv/venta/controller/LectViewController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lv.venta.model.CourseTests;
 import lv.venta.model.Lecturers;
 import lv.venta.model.StudyCourses;
 import lv.venta.service.ILectViewService;
@@ -34,5 +36,22 @@ public class LectViewController {
 		}
 		
 	}
+	//------------------------------------------
+	//-------------AllTestsByCourse-------------
+	@GetMapping("/courses/{courseId}/tests") //localhost:8081/professor/courses
+	public String getControllerCoursesTests(@PathVariable(name = "courseId") long courseId, Model model) {
+		try {
+			List<CourseTests> allTests = lectService.allTestsByCourse(courseId);
+			System.out.println("Atrasti testi: " + allTests.size());
+			model.addAttribute("allTests", allTests);
+		    return "lecturers-courseTests";
+	    
+		}catch(Exception e){
+			model.addAttribute("package", e.getMessage());
+			return "show-error";
+		}
+		
+	}
+	//-------------------------------------------
 	
 }
